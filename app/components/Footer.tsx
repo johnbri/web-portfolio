5d02359374433a93a75e94ef60f32464724f8eb0
John Brink <john-brink@hotmail.com>
2024-02-25 23:40:07 +0100

    Setting up web-portolio git repo

---
+++ app/components/Footer.tsx
@@ -0,0 +1,6 @@
+export default function Footer() {
+    return (
+      <span>Footer</span>
+    );
+  }
+  
---
+++ app/components/Header.tsx
@@ -0,0 +1,6 @@
+export default function Header() {
+    return (
+      <span className="h-10">Header</span>
+    );
+  }
+  
